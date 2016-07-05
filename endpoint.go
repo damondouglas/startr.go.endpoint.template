060e9997b791f9bcd3c9e93ae10251bfcf37a93e
Damon Douglas <douglas.damon@gmail.com>
2016-07-05 15:33:26 -0700

    Insert more boilerplate

--- endpoint.go
+++ endpoint.go
@@ -12,13 +12,33 @@ var (
   audiences       = []string{}
 )
 
-type __service__ struct{}
+type __service__Service struct{}
 
 type List__service__Req struct{}
 
-type __service__List struct{}
+type __service__List struct{
+  Name string `json:"name"`
+}
 
-func (cs *__service__) List__service__(c context.Context, r *List__service__Req) (*__service__List, error) {
+func (cs *__service__Service) List__service__(c context.Context, r *List__service__Req) (*__service__List, error) {
   list := new(__service__List)
+  list.Name = "foo"
   return list, nil
+}
+
+func init() {
+  api, err := endpoints.RegisterService(&__service__Service{}, "__projectname__", "v1", "__service__ API", true)
+  if err != nil {
+    log.Fatalf("Register service: %v", err)
+  }
+
+  list := api.MethodByName("List__service__").Info()
+  // list.Scopes = scopes
+  // list.ClientIds = clientIds
+  list.HTTPMethod = "GET"
+  list.Path = "__projectname__/list"
+  list.Name = "list"
+  list.Desc = "List __projectname__."
+
+  endpoints.HandleHTTP()
 }
